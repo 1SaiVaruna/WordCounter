33b0200c2c96dbcf80b2786f77885d4b28a55736
1SaiVaruna <sai030804@gmail.com>
2024-11-08 15:41:58 +0000

    Fixed a couple of typos.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function absenceOfThreeConsecutiveCharacters(text){
     for ( const character of text){
         const occurrences = Array.from(text).filter(v => v == character).length;
 
-        if (occurences >=3) {
+        if (occurrences >=3) {
             return false;
         }
 
@@ -23,7 +23,7 @@ const wordCountElement = document.querySelector(".word-count");
 const letterCountElement = document.querySelector(".letter-count");
 const spaceCountElement = document.querySelector(".space-count");
 
-text.Input.addEventListener("inpjut", () => {
+text.Input.addEventListener("input", () => {
     const splitted = textInput.value.trim().split(/[\s-]/);
     const letterCount = (textInput.value.match(/[a-z]/gi) || []).length;
     const spaceCount = (text.Input.value.match(/\s+/g) || []).length;
