efdaa726671bd05a1858666686fed7821ec98e7a
1SaiVaruna <sai030804@gmail.com>
2024-11-08 15:20:39 +0000

    Added more code to at least two characters function.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function atLeastTwoCharacters(text){
-    const letters = text.match(/[a-z]/gi)
+    const letters = text.match(/[a-z]/gi);
+
+    return letters.length >= 2;
 }
 
 function absenceOfThreeConsecutiveCharacters(text){
